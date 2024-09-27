1814dca37f43122e26a3833f894ec09e401c05c5
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-09-27 22:04:45 +0200

    first commit, init of the project

---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="font-pp text-4xl">Hello world !</div>;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pp: ['var(--font-pp-book)'],
+        dot: ['var(--font-dotemp-bit)'],
+      },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
