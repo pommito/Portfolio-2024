4c6aaf1112fd983988ca4576ddf585c277d53fba
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-10-01 22:44:55 +0200

    Integ(Playground) : adding image on the shader of Model

--- src/app/playground/components/ProjectImage/shader.ts
+++ src/app/playground/components/ProjectImage/shader.ts
@@ -1,11 +1,17 @@
 export const vertex = `
+varying vec2 vUv;
+
 void main() {
+  vUv = uv;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
 `;
 
 export const fragment = `
+varying vec2 vUv;
+uniform sampler2D uTexture;
 void main() {
-  gl_FragColor = vec4(1., 0., 0., 1.);
+vec4 texture = texture2D(uTexture, vUv);
+  gl_FragColor = texture;
 }
 `;
