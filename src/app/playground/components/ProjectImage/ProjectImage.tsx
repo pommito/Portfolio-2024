2ef36ba24c5bcc7dc43d8676fdbac7974910182e
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-10-01 23:10:51 +0200

    Integ(Playground) : Using useAspect hook from drei to scale model

--- src/app/playground/components/ProjectImage/ProjectImage.tsx
+++ src/app/playground/components/ProjectImage/ProjectImage.tsx
@@ -1,27 +1,22 @@
 import { useThree } from '@react-three/fiber';
-import { useTexture } from '@react-three/drei';
+import { useTexture, useAspect } from '@react-three/drei';
 
 import { fragment, vertex } from './shader';
 import { useRef } from 'react';
 
 export default function Model() {
-  const { viewport } = useThree();
-  const aspectRatio = 16 / 9;
-
-  const x = viewport.width / 2;
-  const y = x / aspectRatio;
-
-  console.log(viewport);
-
   const texture = useTexture('/placeholder.jpg');
+
+  const scale = useAspect(texture.image.width, texture.image.height, 0.5);
+  console.log(scale);
 
   const uniforms = useRef({
     uTexture: { value: texture },
   });
 
   return (
-    <mesh>
-      <planeGeometry args={[x, y, 15, 15]} />
+    <mesh scale={scale}>
+      <planeGeometry args={[1, 1, 15, 15]} />
       <shaderMaterial fragmentShader={fragment} vertexShader={vertex} uniforms={uniforms.current} />
     </mesh>
   );
