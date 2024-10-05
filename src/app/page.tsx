801ef948045a884525b8be0edbd5a8ada6db0086
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-10-05 20:08:48 +0200

    Integ(Home) : some styling changes to make it better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,10 +23,10 @@ export default function Home() {
       <section className="flex flex-col min-h-screen max-h py-20">
         <h3 className="font-dot text-6xl h-fit mb-32">Selected project</h3>
         <div className="grid grid-cols-2 auto-rows-auto gap-8 w-full">
-          <ProjectCard className="" id={1} />
-          <ProjectCard className="" id={2} />
-          <ProjectCard className="" id={3} />
-          <ProjectCard className="" id={4} />
+          <ProjectCard className="mt-40" id={1} />
+          <ProjectCard className="justify-self-end" id={2} />
+          <ProjectCard className="mt-40" id={3} />
+          <ProjectCard className="justify-self-end" id={4} />
         </div>
       </section>
 
