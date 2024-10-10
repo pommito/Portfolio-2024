d1c2ac86113a2b9e0af6d214f88b5b55dc7591f5
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-10-10 10:56:57 +0200

    Styling : Dark mode is now the only mode available on the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 
 import Header from '@/components/Header';
-import Grid from '@/components/Grid';
+// import Grid from '@/components/Grid';
 import Footer from '@/components/Footer';
 import SmoothScroll from '@/components/animations/SmoothScroll';
 
