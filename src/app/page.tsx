fcc551a07bd42ace91799c96c583cfaac7e43137
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-10-18 18:20:46 +0200

    Refacto(Home) : removing the about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,37 +32,6 @@ export default function Home() {
           ))} */}
         </div>
       </section>
-
-      <section className="md:py-20">
-        <h3 className="font-dot text-3xl md:text-6xl mb-8 sm:mb-10">About me</h3>
-        <p className="leading-[150%] text-balance mb-10 sm:text-xl">
-          <TextReveal text={secondParagraph} amountInView={1} />
-        </p>
-        <p className="flex flex-wrap gap-4 md:justify-between">
-          {technos.map((techno, index) => (
-            <span key={index} className="font-dot text-xl sm:text-2xl opacity-50">
-              {techno}
-            </span>
-          ))}
-        </p>
-
-        <h4 className="font-dot opacity-50 mt-8  sm:mt-10 mb-4">[ Work Experiences ]</h4>
-        <ul>
-          {workExperiences.map((exp, index) => (
-            <ResumeListItem key={index} {...exp} />
-          ))}
-        </ul>
-        <h4 className="font-dot opacity-50 mt-8 sm:mt-10 mb-4">[ Education ]</h4>
-        <ul>
-          {educations.map((exp, index) => (
-            <ResumeListItem key={index} {...exp} />
-          ))}
-        </ul>
-        <Link href={'#'} className="flex items-center justify-center gap-2 mt-8  mx-auto w-fit md:mt-20">
-          <span className="font-dot md:text-xl">Download my resume</span>
-          <Image src={downloadIcon} alt="icon in the button for downloading resume" aria-hidden />
-        </Link>
-      </section>
     </main>
   );
 }
