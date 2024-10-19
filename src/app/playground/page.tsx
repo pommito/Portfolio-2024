a0cb3af701e43f77bc86882b0adffd371690cc41
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-10-19 13:23:16 +0200

    Integ(Playground) : integration of a static version of the homepage is done

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,6 +1,4 @@
-'use client';
-
-import { AnimatePresence, motion } from 'framer-motion';
+import Image from 'next/image';
 
 import Grid from '@/components/Grid';
 import Header from '@/components/new/Header';
@@ -11,15 +9,15 @@ export default function Playground() {
       <div className="relative bg-white h-full w-full border rounded-md px-6 py-6">
         <Grid />
         <Header />
-        <main className="w-full h-full">
-          <section className="flex flex-col justify-end gap-6 h-3/5">
+        <main className="grid grid-rows-5 gap-6 w-full h-full">
+          <section className="row-start-1 row-span-3 flex flex-col justify-end gap-6 h-full">
             <h1 className="flex flex-col font-medium text-9xl">
               <span>Front end</span>
               <span className="ml-16">Developer</span>
             </h1>
             <div className="flex justify-between text-xl ">
               <p className="text-balance leading-normal w-2/3">
-                I’m a 24 years old developer based in France who likes to work with React, Wordpress, and Laravel. With
+                I’m a 25 years old developer based in France who likes to work with React, Wordpress, and Laravel. With
                 a keen eye for design, I love crafting immersive and scalable experiences on the web. Together, let’s
                 bring your projects to life.
               </p>
@@ -28,6 +26,20 @@ export default function Playground() {
                 <li>Github</li>
                 <li>Linkedin</li>
               </ul>
+            </div>
+          </section>
+          <section className="row-start-4 row-span-2 grid grid-cols-4 gap-6 w-full">
+            <div className="relative w-full h-full">
+              <Image src="/projects/csf.webp" alt="Chambéry Savoie football" fill className="object-cover" />
+            </div>
+            <div className="relative w-full h-full">
+              <Image src="/projects/csf.webp" alt="Chambéry Savoie football" fill className="object-cover" />
+            </div>
+            <div className="relative w-full h-full">
+              <Image src="/projects/csf.webp" alt="Chambéry Savoie football" fill className="object-cover" />
+            </div>
+            <div className="relative w-full h-full">
+              <Image src="/projects/csf.webp" alt="Chambéry Savoie football" fill className="object-cover" />
             </div>
           </section>
         </main>
--- src/components/new/Header.tsx
+++ src/components/new/Header.tsx
@@ -2,7 +2,7 @@ import DropdownMenu from './DropdownMenu';
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-start">
+    <header className="absolute top-0 left-0 flex justify-between items-start w-full p-6">
       <h2 className="font-medium leading-none uppercase">Victor Lebecq</h2>
       {/* TODO : Enhance the style of the location */}
       <span className="text-xs leading-none uppercase">Grenoble, France</span>
