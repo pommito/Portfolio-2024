dfa27b47f48c21ba0bf66545ee3e7f6821353d87
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-10-21 21:24:06 +0200

    Integ(Works) : structure of the page is done, responsiv too

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -7,17 +7,17 @@ import aboutImage from '/public/about.webp';
 export default function Works() {
   return (
     <main className="grid grid-rows-8 grid-cols-8 gap-4 w-full h-full max-w-[1920px] sm:gap-6 2xl:mx-auto">
-      <section className="flex flex-col gap-3 col-start-1 col-span-8 row-start-2 row-span-2 sm:col-span-5 md:col-span-5">
+      <section className="flex flex-col gap-3 col-start-1 col-span-8 row-start-2 row-span-2 lg:col-span-4">
         <Breadcrumb pageTitle="Works" />
         <h1 className="font-medium text-6xl leading-none">Works</h1>
-        <p className="text-balance leading-normal lg:w-4/5">
+        <p className="text-balance leading-normal">
           These is the full list of all the projects i’ve been working on as a freelance developer. Here you can find
           the link to the live website and the source code if the project is open source. Feel free to browse those
           projects.
         </p>
       </section>
-      <div className="bg-red-200 flex flex-col justify-center col-start-1 col-span-8 row-start-6 row-span-3 mt-6 sm:row-start-5 sm:row-span-4  md:justify-end md:row-start-5 md:row-span-4 lg:col-span-5"></div>
-      <div className="hidden col-start-6 col-span-3 row-start-6 row-span-3 lg:block">
+      <div className="bg-red-200 flex flex-col col-start-1 col-span-8 row-start-4 row-span-5 lg:col-start-5 lg:col-span-4 lg:row-start-2 lg:row-span-7"></div>
+      <div className="hidden col-start-1 col-span-3 row-start-6 row-span-3 lg:block">
         <Image src={aboutImage} alt="placeholder image" objectFit="cover" className="w-full h-full" />
       </div>
     </main>
