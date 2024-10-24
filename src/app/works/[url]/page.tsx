ac5f69ff702c49bab985b40f038d00262ec8684c
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-10-24 20:16:54 +0200

    Integ(works/url) : labels are now integrated in static

--- src/app/works/[url]/page.tsx
+++ src/app/works/[url]/page.tsx
@@ -18,8 +18,35 @@ export default function Page({ params: { url } }: { params: { url: string } }) {
       >
         <Image src={imageTest} alt="placeholder image" className="h-full w-full object-cover" priority />
       </div>
-      <section className="flex flex-col gap-6 col-start-6 col-span-3 row-start-4 row-span-4">
-        <p className="text-balance leading-loose">
+      <div className="flex justify-between gap-6 col-start-6 col-span-3 row-start-4 row-span-1">
+        <div className="w-1/3">
+          <div className="relative after:content-[''] after:absolute after:top-1/2 after:left-0 after:transform after:-translate--1/2 after:w-full after:h-[1px] after:bg-[#0c0c0c]/15 after:-z-1">
+            <h2 className="relative inline-block font-medium bg-white pr-2 z-10">Date</h2>
+          </div>
+          <p className="text-sm opacity-50">Aug. 2024</p>
+        </div>
+        <div className="w-1/3">
+          <div className="relative after:content-[''] after:absolute after:top-1/2 after:left-0 after:transform after:-translate--1/2 after:w-full after:h-[1px] after:bg-[#0c0c0c]/15 after:-z-1">
+            <h2 className="relative inline-block font-medium bg-white pr-2 z-10">Role</h2>
+          </div>
+          <ul className="text-sm opacity-50">
+            <li>Design</li>
+            <li>Developement</li>
+          </ul>
+        </div>
+        <div className="w-1/3">
+          <div className="relative after:content-[''] after:absolute after:top-1/2 after:left-0 after:transform after:-translate--1/2 after:w-full after:h-[1px] after:bg-[#0c0c0c]/15 after:-z-1">
+            <h2 className="relative inline-block font-medium bg-white pr-2 z-10">Techs</h2>
+          </div>
+          <ul className="text-sm opacity-50">
+            <li>Wordpress</li>
+            <li>Javascript</li>
+            <li>Figma</li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex flex-col gap-6 col-start-6 col-span-3 row-start-5 row-span-4">
+        <p className="text-pretty leading-normal">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim
           sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a,
           semper congue, euismod non, mi.
@@ -34,7 +61,7 @@ export default function Page({ params: { url } }: { params: { url: string } }) {
           <SocialLink title="Source code" url="mailto:victor.lebecq.pro@gmail.com" />
           <SocialLink title="Visit website" url="https://github.com/pommito" />
         </ul>
-      </section>
+      </div>
     </main>
   );
 }
