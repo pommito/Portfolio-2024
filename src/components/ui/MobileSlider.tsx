e65bbed92a6040532b153c1f51924b42f334f819
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-10-25 17:54:15 +0200

    Fix(Home) : fixing the height of 100dvh for mobile view

--- src/components/ui/MobileSlider.tsx
+++ src/components/ui/MobileSlider.tsx
@@ -7,7 +7,7 @@ import ProjectSlider from '@/components/common/ProjectSlider';
 
 import { selectedWorks } from '@/constant/selectedWorks';
 
-const DRAG_BUFFER = 50;
+const DRAG_BUFFER = 25;
 
 export default function MobileSlider() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
